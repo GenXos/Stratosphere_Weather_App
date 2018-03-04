5e685fb546cdea04cc6e74406340efe34df4a0a9
Todd Fields <toddwfields@gmail.com>
2018-03-04 12:50:58 -0600

    Update Constants.swift

--- StratosphereWeatherApp/Constants.swift
+++ StratosphereWeatherApp/Constants.swift
@@ -11,6 +11,6 @@ import Foundation
 let URL_BASE = "http://api.openweathermap.org"
 let URL_CURRENT_WEATHER = "/data/2.5/weather"
 
-let API_KEY = "8332d8e9faca580c1e81b1e2cd4d0edd"
+let API_KEY = ""
 
 typealias DownloadComplete = () -> ()
